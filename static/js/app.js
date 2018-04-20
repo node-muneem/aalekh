6e74c6801996e0e49030624f079f81189b8037d2
Amit Gupta <amitgupta.gwl@gmail.com>
2018-04-20 21:08:29 +0530

    complete initial design, and repo setup

---
+++ static/js/app.js
@@ -0,0 +1,19 @@
+var methodColorMapping = {
+    GET : "green",
+    HEAD : "#6ad346",
+    PUT : "orange",
+    POST : "yellow",
+    DELETE : "red",
+    OPTIONS : "#0099f6",
+    PATCH : "cyan",
+    TRACE : "#009c87",
+    CONNECT : "#c574b8",
+    COPY : "#f76aa1",
+    LINK : "#145c85",
+    UNLINK : "#59859e",
+    PURGE : "#ff6633",
+    LOCK : "#25bfdd",
+    UNLOCK : "#008299",
+    PROPFIND : "#00e198",
+    VIEW: "#fec900"
+}
